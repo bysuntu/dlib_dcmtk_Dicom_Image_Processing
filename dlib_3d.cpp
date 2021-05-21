229c983a879f09f1a590f0ad05c785b4553c8a59
hrlsby <bysuntu@users.noreply.github.com>
2021-05-21 11:58:51 +0800

    Create dlib_3d.cpp

---
+++ dlib_3d.cpp
@@ -0,0 +1,10 @@
+#include <dlib\gui_widgets.h>
+#include <dlib\image_transforms.h>
+#include <cmath>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
